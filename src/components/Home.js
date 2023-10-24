3a26b290c94035d48f9e0d4aa4cd1e0b381e397d
lawrahkonwea <lawrahcollections@gmail.com>
2023-10-24 01:57:19 +0100

    new branch

--- src/components/Home.js
+++ src/components/Home.js
@@ -67,7 +67,7 @@ export const Homepage = () => {
               clear as day: to shape the future. Whether you're a coding genius, a
               design maverick, or a concept wizard, you'll have the chance to transform
               your ideas into reality. Solving real-world problems, pushing the boundaries
-              of technology, and creating solutions that can change the world,
+              of technology, and creating solutions that can change the world to a better,
               that's what we're all about!</h4>
             <div className='lensflare'><img src={Lensflare} /></div>
           </div>
